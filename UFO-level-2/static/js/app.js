0d2702cd6c8da86339d6362c488720c84c328254
carolyn-tsai <53666793+carolyn-tsai@users.noreply.github.com>
2020-04-07 15:09:06 -0700

    add javascript to level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,8 +2,6 @@
 
 var tableData = data;
 
-console.log(tableData);
-
 // Line 64 of index.html has <tbody> tag that needs to be selected
 // The data will be inputted into the table body
 
@@ -28,6 +26,61 @@ function create_table(d) {
     
     });
 }
+
+// Create variable for filters
+
+var UFO_filters = {};
+
+// Create funtion to update all user filters
+
+function user_filters() {
+
+    // Once filter is changed, save the element, value, and ID
+
+    var new_element = d3.select(this).select("input");
+    var new_value = new_element.property("value");
+    var new_ID = new_element.attr("id");
+
+    // Once new filter value is entered in the search, add the ID and value to UFO_filters list
+    // If there is no filter added, clear the field
+
+    if (new_value) {
+        UFO_filters[new_ID] = new_value;
+    }
+    else {
+        delete UFO_filters[new_ID];
+    }
+
+    // Apply the function so that all filters will be used to create the updated table
+
+    user_filters();
+
+}
+
+// Create function to create the filtered table
+
+function filter_table() {
+
+    let filter_data = tableData;
+
+    // Create loop that goes through all filters and keeps the data that matches the filter values
+
+    Object.entires(filter).forEach(([key, value]) => {
+        filter_data = filter_data(row => row[key] === value);
+    });
+
+    // Once new filtered data is updated, create new table with those values
+
+    create_table(filter_data);
+}
+
+// Create an event to listen for new filter changes
+
+d3.selectAll(".filter").on("change", user_filters);
+
+// When the page loads, the table will be created
+
+create_table(tableData);
 
 
 
